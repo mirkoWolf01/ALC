712de2e31a549a98b666d85851636e2f895fac36
mirko <mirkowolf46@gmail.com>
2026-08-29 17:25:38 -0300

    Listo para entregar T1_&_T2

---
+++ Taller1/lab1.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+def error(x,y):
+    return abs(np.float64(x) - np.float64(y))
+
+def error_relativo(x,y):
+    if x == 0:
+        return y
+    if y == 0:
+        return x
+
+    return abs(np.float64(x) - np.float64(y)) / abs(np.float64(x))
+
+def matricesIguales(A, B) -> bool:
+    e = 1e-08
+
+    if A.shape != B.shape: return False
+
+    for n in range(A.shape[0]):
+        for m in range(A.shape[1]):
+            if error_relativo(A[n,m], B[n,m]) > e: return False
+
+    return True
+
+def transpuesta(a: np.ndarray) -> np.ndarray:
+    (n, m) = a.shape
+
+    res = []
+    for j in range(m):
+        nueva_fila = []
+        for i in range(n):
+            nueva_fila.append(a[i][j])
+        res.append(nueva_fila)
+
+    return np.array(res)
+
+def esSimetrica(A: np.ndarray) -> bool:
+    (n, m) = A.shape
+    At = transpuesta(A)
+
+    return np.allclose(A, At)
--- alc.py
+++ alc.py
@@ -1,5 +1,48 @@
 import numpy as np
 
+#region Taller1
+def error(x,y):
+    return abs(np.float64(x) - np.float64(y))
+
+def error_relativo(x,y):
+    if x == 0:
+        return y
+    if y == 0:
+        return x
+
+    return abs(np.float64(x) - np.float64(y)) / abs(np.float64(x))
+
+def matricesIguales(A, B) -> bool:
+    e = 1e-08
+
+    if A.shape != B.shape: return False
+
+    for n in range(A.shape[0]):
+        for m in range(A.shape[1]):
+            if error_relativo(A[n,m], B[n,m]) > e: return False
+
+    return True
+
+def transpuesta(a: np.ndarray) -> np.ndarray:
+    (n, m) = a.shape
+
+    res = []
+    for j in range(m):
+        nueva_fila = []
+        for i in range(n):
+            nueva_fila.append(a[i][j])
+        res.append(nueva_fila)
+
+    return np.array(res)
+
+def esSimetrica(A: np.ndarray) -> bool:
+    (n, m) = A.shape
+    At = transpuesta(A)
+
+    return np.allclose(A, At)
+#endregion
+
+#region Taller2
 def rota(theta):
     return np.array([[np.cos(theta), -np.sin(theta)],
                      [np.sin(theta), np.cos(theta)]])
@@ -54,3 +97,5 @@ def mmult(a: np.ndarray, b: np.ndarray):
                 res[i, j] += a[i, k] * b[k, j]
 
     return res
+
+#endregion
